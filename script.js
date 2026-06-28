3d5077a11970a997fb5a983d59a5d9d4cc5eb18b
vkplkmr-dotcom <vkplkmr@gmail.com>
2026-06-29 03:30:34 +0530

    Add script.js Part 1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
