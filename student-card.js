2ef6888161e034cfd4ea88f8507dd32ce71d2ad4
vkplkmr-dotcom <vkplkmr@gmail.com>
2026-07-22 18:53:37 +0530

    Update student-card.js

--- student-card.js
+++ student-card.js
@@ -1,1 +1,125 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+    getFirestore,
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
+// Firebase Config
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+projectId: "YOUR_PROJECT_ID"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+// URL से Student ID लेना
+
+const params = new URLSearchParams(window.location.search);
+
+const studentId = params.get("id");
+
+
+if(!studentId){
+
+alert("Student ID Missing");
+
+}
+
+
+
+// Firestore से Data लाना
+
+async function loadStudent(){
+
+const ref = doc(db,"qrData",studentId);
+
+const snap = await getDoc(ref);
+
+
+if(snap.exists()){
+
+
+const data = snap.data();
+
+
+// HTML में data दिखाना
+
+document.getElementById("studentName").innerHTML =
+data.name || "No Name";
+
+
+document.getElementById("studentId").innerHTML =
+studentId;
+
+
+document.getElementById("studentCourse").innerHTML =
+data.program || "Course";
+
+
+if(data.expiryDate){
+
+let date=data.expiryDate.toDate();
+
+document.getElementById("expiryDate").innerHTML =
+date.toLocaleDateString();
+
+}
+
+
+
+// Status
+
+let status=document.getElementById("status");
+
+
+if(data.active){
+
+status.innerHTML="ACTIVE";
+status.className="active";
+
+}else{
+
+status.innerHTML="INACTIVE";
+status.className="inactive";
+
+}
+
+
+// QR Code
+
+new QRCode(document.getElementById("qrcode"),{
+
+text:
+window.location.href,
+
+width:120,
+height:120
+
+});
+
+
+}
+else{
+
+document.getElementById("studentName").innerHTML =
+"Student Not Found";
+
+}
+
+
+}
+
+
+loadStudent();
