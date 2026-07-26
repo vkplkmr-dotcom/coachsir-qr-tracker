019c4c8d19257a59d16a99041c2bfa6c61100842
vkplkmr-dotcom <vkplkmr@gmail.com>
2026-07-26 13:15:02 +0530

    Update student-card.js
    Signed-off-by: vkplkmr-dotcom <vkplkmr@gmail.com>

--- student-card.js
+++ student-card.js
@@ -1,6 +1,6 @@
 // Test Student ID
-const studentId = "S001";
-
+const urlParams = new URLSearchParams(window.location.search);
+const studentId = urlParams.get("id");
 // Load Student Data
 db.collection("qrData").doc(studentId).get()
 .then((doc) => {
@@ -24,12 +24,20 @@ db.collection("qrData").doc(studentId).get()
 
     // Expiry Date
     if (data.expiryDate) {
-        const date = data.expiryDate.toDate();
+       let expiry = data.expiryDate;
 
-        document.getElementById("expiry").innerText =
-            date.toLocaleDateString("en-IN");
+if (expiry) {
+
+    if (expiry.toDate) {
+        expiry = expiry.toDate();
+    } else {
+        expiry = new Date(expiry);
     }
 
+    document.getElementById("expiry").innerText =
+        expiry.toLocaleDateString("en-IN");
+}
+        
     // Payment Status
     document.getElementById("paymentStatus").innerText =
         (data.paymentStatus || "").toUpperCase();
@@ -49,13 +57,13 @@ db.collection("qrData").doc(studentId).get()
     // Student Photo
     const photo = document.getElementById("studentPhoto");
 
-    photo.src = "assets/students/" + studentId + ".jpg";
-
-    photo.onerror = function () {
-        photo.src = "assets/student.png";
-    };
-
+    if (data.photoURL) {
+    photo.src = data.photoURL;
+} else {
+    photo.src = "assets/student.png";
+}
     // QR Code
+    document.getElementById("qrcode").innerHTML = "";
     new QRCode(document.getElementById("qrcode"), {
         text: studentId,
         width: 180,
