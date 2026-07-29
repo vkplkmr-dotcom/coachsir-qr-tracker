783e7f9544def8767f5e135dafb7fe16c5f040c7
vkplkmr-dotcom <vkplkmr@gmail.com>
2026-07-29 17:00:35 +0530

    Update settings.js
    Signed-off-by: vkplkmr-dotcom <vkplkmr@gmail.com>

--- admin/settings.js
+++ admin/settings.js
@@ -1,1 +1,33 @@
+const db = firebase.firestore();
 
+async function loadSettings() {
+
+    const doc = await db
+    .collection("settings")
+    .doc("payment")
+    .get();
+
+    if(doc.exists){
+        document.getElementById("paymentAmount").value =
+        doc.data().amount || 30;
+    }
+}
+
+async function savePaymentSettings(){
+
+    const amount = Number(
+        document.getElementById("paymentAmount").value
+    );
+
+    await db
+    .collection("settings")
+    .doc("payment")
+    .set({
+        amount: amount
+    });
+
+    document.getElementById("statusMsg").innerHTML =
+    "✅ Settings Saved Successfully";
+}
+
+loadSettings();
